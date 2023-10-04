597c480290a7614f1ff31906096fbc9b034e328d
LUCAS CALIXTO DOS SANTOS <20221170150032@ifrn.local>
2023-10-04 10:04:20 -0300

    mecânica do pulo e câmera

---
+++ trabalho de motores/Assets/Scripts/jogador.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class jogador : MonoBehaviour
+{
+    public int velocidade = 10;
+    public Rigidbody rb;
+    public int forcaPulo = 7;
+    public bool noChao;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        TryGetComponent(out rb);
+
+
+
+    }
+        private void OnCollisionEnter(Collision collision){
+            if (!noChao && collision.gameObject.tag == "Chão"){
+                noChao = true;
+            }
+        }
+    // Update is called once per frame
+    void Update()
+    {
+
+        float H = Input.GetAxis("Horizontal");
+        float V = Input.GetAxis("Vertical");
+        if(Input.GetKeyDown(KeyCode.Space) && noChao){
+
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+            noChao = false;
+        }
+
+       Vector3 direcao = new Vector3(H,0,V);
+
+        rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+
+        if(transform.position.y <= 0) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+    }
+}
